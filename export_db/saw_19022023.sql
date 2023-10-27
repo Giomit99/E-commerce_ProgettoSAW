f9c40ad6bdb8176789724770bcfbe2d68a2b7eb8
Giomit99 <mitkovgjorche@gmail.com>
2023-10-27 14:01:19 +0200

    modifica export file sql

--- export_db/saw_19022023.sql
+++ export_db/saw_19022023.sql
@@ -1,29 +1,79 @@
--- MySQL dump 10.13  Distrib 8.0.25, for macos11 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: saw
--- ------------------------------------------------------
--- Server version	5.7.39
+-- Host: localhost:3306
+-- Creato il: Ott 27, 2023 alle 12:46
+-- Versione del server: 10.4.25-MariaDB
+-- Versione PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Table structure for table `prodotti`
+-- Database: `startsaw`
 --
 
-DROP TABLE IF EXISTS `prodotti`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `acquisti`
+--
+
+CREATE TABLE `acquisti` (
+  `id_user` int(11) NOT NULL,
+  `id_prodotto` int(11) NOT NULL,
+  `quantita` int(11) NOT NULL,
+  `prezzo` int(11) NOT NULL,
+  `data` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dump dei dati per la tabella `acquisti`
+--
+
+INSERT INTO `acquisti` (`id_user`, `id_prodotto`, `quantita`, `prezzo`, `data`) VALUES
+(2, 7, 1, 20, '2023-10-27'),
+(2, 7, 1, 20, '2023-10-27'),
+(2, 7, 1, 20, '2023-10-27'),
+(2, 7, 1, 20, '2023-10-27'),
+(2, 7, 1, 20, '2023-10-27'),
+(2, 7, 1, 20, '2023-10-27'),
+(2, 8, 1, 22, '2023-10-27'),
+(2, 9, 1, 35, '2023-10-27'),
+(2, 7, 1, 20, '2023-10-27'),
+(2, 7, 1, 20, '2023-10-27'),
+(2, 7, 1, 20, '2023-10-27'),
+(2, 7, 1, 20, '2023-10-27'),
+(2, 7, 1, 20, '2023-10-27'),
+(2, 7, 1, 20, '2023-10-27'),
+(2, 7, 1, 20, '2023-10-27'),
+(2, 7, 1, 20, '2023-10-27'),
+(2, 7, 1, 20, '2023-10-27'),
+(2, 8, 1, 22, '2023-10-27'),
+(2, 7, 1, 20, '2023-10-27'),
+(2, 7, 1, 20, '2023-10-27'),
+(2, 7, 1, 20, '2023-10-27'),
+(2, 8, 1, 22, '2023-10-27'),
+(2, 8, 1, 22, '2023-10-27'),
+(2, 7, 1, 20, '2023-10-27'),
+(2, 7, 1, 20, '2023-10-27');
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `prodotti`
+--
+
 CREATE TABLE `prodotti` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `descrizione` varchar(45) DEFAULT NULL,
   `marca` varchar(45) DEFAULT NULL,
   `sesso` varchar(10) DEFAULT NULL,
@@ -31,30 +81,28 @@ CREATE TABLE `prodotti` (
   `prezzo` int(11) DEFAULT NULL,
   `quantità` int(4) DEFAULT NULL,
   `data_modifica` datetime DEFAULT NULL,
-  `data_aggiunta` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `data_aggiunta` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `prodotti`
+-- Dump dei dati per la tabella `prodotti`
 --
 
-LOCK TABLES `prodotti` WRITE;
-/*!40000 ALTER TABLE `prodotti` DISABLE KEYS */;
-INSERT INTO `prodotti` VALUES (7,'polo','nordica','uomo','abbigliamento',20,NULL,NULL,NULL),(8,'polo','kalengi','donna','abbigliamento',22,NULL,NULL,NULL),(9,'pantaloni','levis','uomo','abbigliamento',35,NULL,NULL,NULL),(10,'maglietta','rams','uomo','abbigliamento',30,NULL,NULL,NULL),(11,'sci','nordica','donna','attrezzatura',150,NULL,NULL,NULL);
-/*!40000 ALTER TABLE `prodotti` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `prodotti` (`id`, `descrizione`, `marca`, `sesso`, `tipologia`, `prezzo`, `quantità`, `data_modifica`, `data_aggiunta`) VALUES
+(7, 'polo', 'nordica', 'uomo', 'abbigliamento', 20, NULL, NULL, NULL),
+(8, 'polo', 'kalengi', 'donna', 'abbigliamento', 22, NULL, NULL, NULL),
+(9, 'pantaloni', 'levis', 'uomo', 'abbigliamento', 35, NULL, NULL, NULL),
+(10, 'maglietta', 'rams', 'uomo', 'abbigliamento', 30, NULL, NULL, NULL),
+(11, 'sci', 'nordica', 'donna', 'attrezzatura', 150, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struttura della tabella `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `iduser` int(11) NOT NULL AUTO_INCREMENT,
+  `iduser` int(11) NOT NULL,
   `firstname` char(25) NOT NULL,
   `lastname` char(25) NOT NULL,
   `email` char(40) NOT NULL,
@@ -67,28 +115,50 @@ CREATE TABLE `users` (
   `Citta` varchar(20) DEFAULT NULL,
   `Indirizzo` varchar(40) DEFAULT NULL,
   `Cap` int(5) DEFAULT NULL,
-  `Newsletter` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`iduser`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `Newsletter` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `users`
+-- Dump dei dati per la tabella `users`
 --
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Francesco','Filippone','fran.filippone@gmail.com','$2y$10$9AKURihClcqjcqZXXwyn4u2FxaUSstV2ZgT0kJfDH5aBPT/5ScBAS',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `users` (`iduser`, `firstname`, `lastname`, `email`, `pass`, `sesso`, `DataDiNascita`, `Telefono`, `Stato`, `Provincia`, `Citta`, `Indirizzo`, `Cap`, `Newsletter`) VALUES
+(1, 'Francesco', 'Filippone', 'fran.filippone@gmail.com', '$2y$10$9AKURihClcqjcqZXXwyn4u2FxaUSstV2ZgT0kJfDH5aBPT/5ScBAS', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(2, 'Sports', 'Unlimited', 'sportsunlimitedsaw@gmail.com', '$2y$10$ocNRXXEkLUDG4qLomDVcveoNobDTtqNkJRMNlb8QWi4fbcyaA3m/q', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `prodotti`
+--
+ALTER TABLE `prodotti`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`iduser`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `prodotti`
+--
+ALTER TABLE `prodotti`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT per la tabella `users`
+--
+ALTER TABLE `users`
+  MODIFY `iduser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-02-19  0:03:58
